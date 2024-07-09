888336bd375270f210b4e780cd653aae9c91719b
Cynthia Seidel <cynthia_sdl@MacBook-Air-von-Cynthia.local>
2024-07-08 23:53:03 -0400

    let' go

---
+++ html_flags_and_colors/quiz_script.js
@@ -0,0 +1,16 @@
+function checkAnswer(answer) {
+    var correctAnswer = "blue and white"; // Die richtige Antwort
+    var resultDiv = document.getElementById('result');
+    var flagDiv = document.getElementById('flag');
+
+    if (answer === correctAnswer) {
+        resultDiv.innerHTML = "Correct!";
+        flagDiv.innerHTML = '<img src="flag-antarctica.png" alt="Antarctic Flag">';
+    } else {
+        resultDiv.innerHTML = "Wrong! It's blue and white";
+        flagDiv.innerHTML = '<img src="flag-antarctica.png" alt="Antarctic Flag">';
+    }
+
+    // Zeige die Flagge an, unabhängig von der Antwort
+    flagDiv.style.display = 'block';
+}
